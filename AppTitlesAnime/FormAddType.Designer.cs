6e19db3ab2bdf65c9ae06b7327188d21adaa4168
Uliana <ulianashukshina@mail.ru>
2025-04-08 12:32:15 +0500

    Созданаформа FormAddTypes, добавлены кнопки сохранить и отправить(lable1)

---
+++ AppTitlesAnime/FormAddType.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace AppTitlesAnime
+{
+    partial class FormAddType
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanelBottom = new FlowLayoutPanel();
+            btnSaveChanges = new Button();
+            btnCancel = new Button();
+            panel1 = new Panel();
+            panelFill = new Panel();
+            label1 = new Label();
+            flowLayoutPanelBottom.SuspendLayout();
+            panelFill.SuspendLayout();
+            SuspendLayout();
+            // 
+            // flowLayoutPanelBottom
+            // 
+            flowLayoutPanelBottom.AutoSize = true;
+            flowLayoutPanelBottom.Controls.Add(btnSaveChanges);
+            flowLayoutPanelBottom.Controls.Add(btnCancel);
+            flowLayoutPanelBottom.Controls.Add(panel1);
+            flowLayoutPanelBottom.Dock = DockStyle.Bottom;
+            flowLayoutPanelBottom.Location = new Point(0, 417);
+            flowLayoutPanelBottom.Name = "flowLayoutPanelBottom";
+            flowLayoutPanelBottom.Padding = new Padding(10);
+            flowLayoutPanelBottom.Size = new Size(706, 67);
+            flowLayoutPanelBottom.TabIndex = 1;
+            // 
+            // btnSaveChanges
+            // 
+            btnSaveChanges.AutoSize = true;
+            btnSaveChanges.Location = new Point(13, 13);
+            btnSaveChanges.Name = "btnSaveChanges";
+            btnSaveChanges.Size = new Size(115, 35);
+            btnSaveChanges.TabIndex = 2;
+            btnSaveChanges.Text = "Сохранить";
+            btnSaveChanges.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            btnCancel.AutoSize = true;
+            btnCancel.Location = new Point(134, 13);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(88, 35);
+            btnCancel.TabIndex = 3;
+            btnCancel.Text = "Отмена";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(13, 54);
+            panel1.Name = "panel1";
+            panel1.Padding = new Padding(10);
+            panel1.Size = new Size(706, 0);
+            panel1.TabIndex = 1;
+            // 
+            // panelFill
+            // 
+            panelFill.Controls.Add(label1);
+            panelFill.Dock = DockStyle.Fill;
+            panelFill.Location = new Point(0, 0);
+            panelFill.Name = "panelFill";
+            panelFill.Padding = new Padding(10);
+            panelFill.Size = new Size(706, 417);
+            panelFill.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Top;
+            label1.Location = new Point(10, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 25);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // FormAddType
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(706, 484);
+            Controls.Add(panelFill);
+            Controls.Add(flowLayoutPanelBottom);
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(5);
+            Name = "FormAddType";
+            Text = "FormAddType";
+            flowLayoutPanelBottom.ResumeLayout(false);
+            flowLayoutPanelBottom.PerformLayout();
+            panelFill.ResumeLayout(false);
+            panelFill.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private FlowLayoutPanel flowLayoutPanelBottom;
+        private Panel panel1;
+        private Panel panelFill;
+        private Button btnSaveChanges;
+        private Button btnCancel;
+        private Label label1;
+    }
+}
